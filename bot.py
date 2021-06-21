d98265863fd1ea52cc91c2c48df6800adb120000
dablenparty <hdavenport8902@gmail.com>
2021-06-21 12:06:51 -0400

    No longer double sends insults when commands are used

--- bot.py
+++ bot.py
@@ -45,11 +45,12 @@ if __name__ == "__main__":
     load_dotenv()
     intents = discord.Intents.default()
     intents.members = True
-    bot = InsultBot(intents=intents, command_prefix="?")
+    cmd_prefix = "?"
+    bot = InsultBot(intents=intents, command_prefix=cmd_prefix)
 
     @bot.listen('on_message')
     async def on_message(message: discord.Message, command=False):
-        if message.author == bot.user:
+        if message.author == bot.user or (not command and message.content.startswith(cmd_prefix)):
             return
 
         if command or not random.randint(0, 100) % 15:
