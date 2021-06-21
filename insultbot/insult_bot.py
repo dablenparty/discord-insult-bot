622c0361b7ac33203d16ddbff4914d20d0d3e77c
dablenparty <hdavenport8902@gmail.com>
2021-06-21 15:32:47 -0400

    Added ability to "hot update" insults file so that it can be updated on the fly

--- insultbot/insult_bot.py
+++ insultbot/insult_bot.py
@@ -8,21 +8,29 @@ from pathlib import Path
 
 
 class InsultBot(commands.Bot):
-    __slots__ = ["_token", "_insults", "_custom_users"]
+    __slots__ = ["_token", "_insults", "_custom_users", "_insults_file", "_last_file_mtime"]
 
     def __init__(self, **options):
         super().__init__(**options)
         self._token = os.getenv("DISCORD_TOKEN")
-        parent_dir = Path(__file__).parent
-        with (parent_dir / "insults.json").open() as json_stream:
+        self._insults_file = Path(__file__).parent / "insults.json"
+        self._load_insults()
+
+    def _load_insults(self):
+        with self._insults_file.open() as json_stream:
             data: dict = json.load(json_stream)
             self._insults = data.get("generic")
             self._custom_users: dict = data.get("users")
+        self._last_file_mtime = os.path.getmtime(self._insults_file)
 
     async def on_ready(self):
         print(f"{self.user} v{__version__} has loaded in the following guilds:")
         for guild in self.guilds:
             print(f" - {guild.name} ({guild.id})\n")
+
+    def _check_mtime(self):
+        if os.path.getmtime(self._insults_file) != self._last_file_mtime:
+            self._load_insults()
 
     async def insult(self, channel: discord.TextChannel, user: discord.User):
         """
@@ -31,6 +39,8 @@ class InsultBot(commands.Bot):
         :param channel: Text channel to send message in
         :param user: User to insult
         """
+        self._check_mtime()  # on every message, check if new insults need to be loaded or not
+
         user_id_string = f"<@{user.id}>"
         chosen_insult = ""
 
@@ -40,7 +50,8 @@ class InsultBot(commands.Bot):
 
         while not chosen_insult.strip():
             chosen_insult = random.choice(self._insults)
-        chosen_insult = chosen_insult.replace("{user}", user_id_string) if "{user}" in chosen_insult else user_id_string + " " + chosen_insult
+        chosen_insult = chosen_insult.replace("{user}", user_id_string) if "{user}" in chosen_insult \
+            else user_id_string + " " + chosen_insult
         await channel.send(chosen_insult)
 
     def launch(self):
