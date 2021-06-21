95c65fe6319386f8446e93198432de179120d0cd
dablenparty <hdavenport8902@gmail.com>
2021-06-21 12:14:56 -0400

    Moved InsultBot class to insultbot package

--- bot.py
+++ bot.py
@@ -2,44 +2,9 @@
 import random
 import discord
 from discord.ext import commands
-import os
 from dotenv import load_dotenv
-from pathlib import Path
-import json
 
-
-class InsultBot(commands.Bot):
-    __slots__ = ["_token", "_insults", "_custom_users"]
-
-    def __init__(self, **options):
-        super().__init__(**options)
-        self._token = os.getenv("DISCORD_TOKEN")
-        parent_dir = Path(__file__).parent
-        with (parent_dir / "insults.json").open() as json_stream:
-            data: dict = json.load(json_stream)
-            self._insults = data.get("generic")
-            self._custom_users: dict = data.get("users")
-
-    async def on_ready(self):
-        print(f"{self.user} has found these users:")
-        guild = self.guilds[0]
-        members = [str(member.id) + ": " + member.name for member in guild.members]
-        print('\n - '.join(members))
-
-    async def insult(self, channel: discord.TextChannel, user: discord.User):
-        user_id_string = f"<@{user.id}>"
-        insults = self._insults
-        chosen_insult = ""
-        if str(user.id) in self._custom_users.keys():
-            insults.extend(self._custom_users.get(str(user.id), "").get("insults"))
-        while not chosen_insult.strip():
-            chosen_insult = random.choice(self._insults)
-        chosen_insult = chosen_insult.replace("{user}", user_id_string) if "{user}" in chosen_insult else user_id_string + " " + chosen_insult
-        await channel.send(chosen_insult)
-
-    def launch(self):
-        self.run(self._token)
-
+from insultbot.insult_bot import InsultBot
 
 if __name__ == "__main__":
     load_dotenv()
