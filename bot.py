c067d11185911b6e57ca929585f990fbacd8826f
dablenparty <hdavenport8902@gmail.com>
2021-06-21 20:51:14 -0400

    Added addinsult command, changed prefix to '/' for use later with Slash Commands

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ def main():
     load_dotenv()
     intents = discord.Intents.default()
     intents.members = True
-    cmd_prefix = "?"
+    cmd_prefix = "/"
     bot = InsultBot(intents=intents, command_prefix=cmd_prefix)
 
     @bot.listen('on_message')
@@ -25,6 +25,19 @@ def main():
     @bot.command(name="insult")
     async def insult_command(ctx: commands.Context):
         await on_message(ctx.message, True)
+
+    @bot.command(name="addinsult")
+    async def add_insult(ctx: commands.Context):
+        message: discord.Message = ctx.message
+        if len(message.mentions) != 1 or message.mention_everyone:
+            await ctx.send(f"Usage: `{cmd_prefix}addinsult user insult`")
+            return
+        user: discord.User = message.mentions[0]
+        insult = message.content\
+            .replace(f"{cmd_prefix}{ctx.command} ", "")\
+            .replace(f"<@!{user.id}> ", "", 1)\
+            .replace(f"<@!{user.id}> ", "{user}")
+        bot.add_insult_to_user(user, insult)
 
     bot.launch()
 
