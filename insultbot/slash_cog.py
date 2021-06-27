16f27b30f2c0d6a31988cd64be3b82bf6a96cfea
dablenparty <hdavenport8902@gmail.com>
2021-06-27 02:59:22 -0400

    You can now sick the bot on other people with the command

--- insultbot/slash_cog.py
+++ insultbot/slash_cog.py
@@ -1,7 +1,7 @@
 # slash_cog.py
-
+import discord
 from discord.ext import commands
-from discord_slash import cog_ext, SlashContext
+from discord_slash import cog_ext, SlashContext, manage_commands
 from .insult_bot import InsultBot
 
 
@@ -9,9 +9,15 @@ class SlashCog(commands.Cog):
     def __init__(self, bot: InsultBot):
         self.bot: InsultBot = bot
 
-    @cog_ext.cog_slash(name="insult")
-    async def _insult_command(self, ctx: SlashContext):
-        await self.bot.insult(ctx.channel, ctx.author)
+    @cog_ext.cog_slash(name="insult",
+                       description="Asks the bot to insult you, or others...",
+                       options=[manage_commands.create_option("user",
+                                                              "The user you wish to sick the bot on",
+                                                              discord.User,
+                                                              False)])
+    async def _insult_command(self, ctx: SlashContext, tagged_user: discord.User = None):
+        user = ctx.author if tagged_user is None else tagged_user
+        await self.bot.insult(ctx.channel, user)
 
 
 def setup(bot: commands.Bot):
