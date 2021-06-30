df4a568b433a6cc0230ea1fa11336ee2414dbf2f
dablenparty <57240002+dablenparty@users.noreply.github.com>
2021-06-30 12:06:36 -0400

    Moved url params to method params instead of instance vars

--- insultbot/insult_api.py
+++ insultbot/insult_api.py
@@ -4,19 +4,25 @@ import requests as req
 
 class InsultApi:
     """Wrapper class for https://insult.mattbas.org/api/"""
-    __slots__ = ["_file_format", "_link", "_template", "_plural"]
+    __slots__ = ["_file_format", "_link"]
 
-    def __init__(self, file_format="txt", template: str = None, plural=False):
+    def __init__(self, file_format="txt"):
         self._file_format = file_format
-        self._template = template
-        self._plural = plural
         self._link = f"https://insult.mattbas.org/api/insult.{self._file_format}"
 
-    def get_insult(self):
-        params = {"who": "%7Buser%7D"}
-        if self._template is not None:
-            params["template"] = self._template
-        if self._plural:
+    def get_insult(self, who="%7Buser%7D", plural=False, template: str = None):
+        """
+        Requests an insult from the API
+
+        :param who: Who to insult
+        :param plural: Whether or not to insult multiple people
+        :param template: Template to follow (recommend leaving this as None)
+        :return: Formatted insult
+        """
+        params = {"who": who}
+        if template is not None:
+            params["template"] = template
+        if plural:
             params["plural"] = "on"
         print("Requesting insult from API...")
         response = req.get(self._link, params=params)
