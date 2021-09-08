979f8cec831c636cad6da000e2a18a9641732d4a
dablenparty <hdavenport8902@gmail.com>
2021-09-08 14:50:48 -0400

    Created registerSlashCommands method

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,7 +1,23 @@
-import { CommandInteraction } from "discord.js";
+import { Client, Collection, CommandInteraction } from "discord.js";
 
 export type Command = {
   name: string;
   description: string;
   execute(interaction: CommandInteraction): Promise<void>;
 };
+
+export async function registerSlashCommands(
+  commands: Collection<string, Command>,
+  client: Client
+) {
+  for (const [, command] of commands) {
+    const app =
+      process.env.DEV === "1"
+        ? client.guilds.cache.get(process.env.GUILD_ID || "")
+        : client.application;
+    await app?.commands.create({
+      name: command.name,
+      description: command.description,
+    });
+  }
+}
