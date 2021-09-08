911f3b22a5218cfe94dc50dbcdeb9fded126880e
dablenparty <hdavenport8902@gmail.com>
2021-09-08 14:01:24 -0400

    Added documentation to getInsult

--- src/insultbot/insultApi.ts
+++ src/insultbot/insultApi.ts
@@ -1,6 +1,12 @@
 import axios from "axios";
 
-export default async function getInsult(who = "{user}") {
+/**
+ * Gets an insult from the API found at https://insult.mattbas.org
+ *
+ * @param who Who to insult
+ * @returns Formatted insult
+ */
+export default async function getInsult(who = "{user}"): Promise<string> {
   const response = await axios.get(
     "https://insult.mattbas.org/api/insult.txt",
     { params: { who } }
