a98c7bc15f5a3bc675de1952da824f8591393867
dablenparty <hdavenport8902@gmail.com>
2021-09-08 14:34:28 -0400

    Created generateInsult method to format insult properly

---
+++ src/insultbot/insultBot.ts
@@ -0,0 +1,17 @@
+import { User } from "discord.js";
+import getInsult from "./insultApi";
+
+/**
+ * Generates and formats an insult for the bot to send
+ *
+ * @param user User to insult
+ * @returns Formatted insult
+ */
+export async function generateInsult(user: User): Promise<string> {
+  if (!(Math.floor(Math.random() * 9) % 9)) return "🖕";
+  try {
+    return (await getInsult()).replace("{user}", user.toString);
+  } catch (e) {
+    return "🖕"; // failsafe in case the insult API gets screwed up or goes down
+  }
+}
