fb8635834a04b94423a0c3b02834511b7fb69240
dablenparty <hdavenport8902@gmail.com>
2021-09-08 14:57:43 -0400

    Added readCommandsFromFolder method to dynamically load slash commands

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,4 +1,6 @@
 import { Client, Collection, CommandInteraction } from "discord.js";
+import { readdir } from "fs/promises";
+import { join } from "path";
 
 export type Command = {
   name: string;
@@ -6,10 +8,30 @@ export type Command = {
   execute(interaction: CommandInteraction): Promise<void>;
 };
 
+export async function readCommandsFromFolder(): Promise<
+  Collection<string, Command>
+> {
+  const commands = new Collection<string, Command>();
+  try {
+    const commandsFolderPath = join(__dirname, "commands");
+    const files = await readdir(commandsFolderPath);
+    for (const file of files.filter((file) => file.endsWith(".js"))) {
+      const {
+        command,
+      }: { command: Command } = require(`${commandsFolderPath}/${file}`);
+      commands.set(command.name, command);
+    }
+  } catch (e) {
+    console.error("There was an error reading command files");
+    console.error(e);
+  }
+  return commands;
+}
+
 export async function registerSlashCommands(
   commands: Collection<string, Command>,
   client: Client
-) {
+): Promise<void> {
   for (const [, command] of commands) {
     const app =
       process.env.DEV === "1"
