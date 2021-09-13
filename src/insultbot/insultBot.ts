817929c0ff3047827c6737326f4e84fa7ea73f7f
dablenparty <hdavenport8902@gmail.com>
2021-09-13 08:58:56 -0400

    Re-added the ability to tag users

--- src/insultbot/insultBot.ts
+++ src/insultbot/insultBot.ts
@@ -1,4 +1,5 @@
 import getInsult from "./insultApi";
+import { User } from "discord.js";
 
 /**
  * Generates and formats an insult for the bot to send
@@ -8,10 +9,12 @@ import getInsult from "./insultApi";
  *
  * @returns Formatted insult
  */
-export async function generateInsult(): Promise<string> {
+export async function generateInsult(user?: User): Promise<string> {
   if (!(Math.floor(Math.random() * 9) % 9)) return "🖕";
   try {
-    return await getInsult();
+    if (!user) return await getInsult();
+    const insult = await getInsult("{user}", false);
+    return insult.replace("{user}", user.toString());
   } catch (e) {
     return "🖕"; // failsafe in case the insult API gets screwed up or goes down
   }
