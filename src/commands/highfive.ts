59661df5636081916efacfaad8f6ac49735d2d73
dablenparty <hdavenport8902@gmail.com>
2022-02-22 23:41:26 -0500

    Renamed command to High Five

--- src/commands/highfive.ts
+++ src/commands/highfive.ts
@@ -2,8 +2,8 @@ import { CommandInteraction } from "discord.js";
 import { SlashCommandBuilder } from "@discordjs/builders";
 
 export const data = new SlashCommandBuilder()
-  .setName("dapmeup")
-  .setDescription("Dap up the bot");
+  .setName("highfive")
+  .setDescription("High five the bot");
 
 export async function execute(interaction: CommandInteraction) {
   await interaction.reply({ content: "👏" });
